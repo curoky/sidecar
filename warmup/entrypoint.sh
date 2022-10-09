a02b545ad94d817359c0a10bfc7d5e8a3b38e4dc
curoky <cccuroky@gmail.com>
2022-10-09 23:01:06 +0800

    feat(warmup): dump script log to file

--- warmup/entrypoint.sh
+++ warmup/entrypoint.sh
@@ -20,32 +20,37 @@ set -xeuo pipefail
 
 sudo service ssh start
 
+if [[ ! -d /var/log/app/warmup ]]; then
+  sudo mkdir -p /var/log/app/warmup
+fi
+sudo chown -R door:door /var/log/app/warmup
+
 while ! nc -zv redis 6379; do
   sleep 1
 done
-/opt/redis/warmup/test-set-get.py
+/opt/redis/warmup/test-set-get.py >/var/log/app/warmup/redis.log 2>&1
 
 while ! nc -zv mongo 27017; do
   sleep 1
 done
-/opt/mongodb/warmup/test-set-get.py
+/opt/mongodb/warmup/test-set-get.py >/var/log/app/warmup/mongodb.log 2>&1
 
 while ! nc -zv kafka 40800; do
   sleep 1
 done
 
-/opt/kafka/warmup/create-test-topic.py
-/opt/kafka/warmup/test-producer.py &
-/opt/kafka/warmup/test-consumer.py &
+/opt/kafka/warmup/create-test-topic.py >/var/log/app/warmup/kafka-create-test-topic.log 2>&1
+/opt/kafka/warmup/test-producer.py >/var/log/app/warmup/kafka-producer.log 2>&1 &
+/opt/kafka/warmup/test-consumer.py >/var/log/app/warmup/kafka-consumer.log 2>&1 &
 
 while ! nc -zv hadoop 42016; do
   sleep 1
 done
-/opt/hadoop/warmup/prepare-test-data.sh
+/opt/hadoop/warmup/prepare-test-data.sh >/var/log/app/warmup/hadoop.log 2>&1
 
 while ! nc -zv spark 43201; do
   sleep 1
 done
-/opt/spark/warmup/word_count.py /LICENSE.txt
+/opt/spark/warmup/word_count.py /LICENSE.txt >/var/log/app/warmup/spark.log 2>&1
 
 while true; do sleep 1000; done
